0069c33324a7e1aad440815d9330b224f912baca
sandra <sandrahpostawa@gmail.com>
2024-08-05 01:05:31 +0200

    Activity: added audit logs

---
+++ components/activity-item.tsx
@@ -0,0 +1,31 @@
+import { AuditLog } from "@prisma/client";
+import { generateLogMessage } from "@/lib/generate-log-message";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { format } from "date-fns";
+
+interface ActivityItemProps {
+    data: AuditLog;
+}
+
+export const ActivityItem = ({
+    data,
+}: ActivityItemProps) => {
+    return (
+        <li className="flex items-center gap-x-2">
+            <Avatar className="h-8w-8">
+                <AvatarImage src={data.userImage} />
+            </Avatar>
+            <div className="flex flex-col space-y-0.5">
+                <p className="text-sm text-muted-foreground">
+                    <span className="font-semibold lowercase text-neutral-700">
+                        {data.userName}
+                    </span> {generateLogMessage(data)}
+                </p>
+                <p className="mt-2 space-y-4 text-xs text-muted-foreground"  >
+                    {format(new Date(data.createdAt), "MMM dd, yyyy 'at' hh:mm a")}
+                </p>
+            </div>
+
+        </li>
+    );
+};
